38ae118fe97da7b6690df56fe5104a7b7df171b5
Darshan Vs <109671745+drsnvs@users.noreply.github.com>
2022-09-15 09:48:31 +0530

    Wrong Project

--- ResumeApp/static/assets/js/forms/forms-text-editor.js
+++
@@ -1,9 +0,0 @@
-// Markdown Editor
-new SimpleMDE({
-	element: document.getElementById("demo1"),
-	spellChecker: false,
-});
-// Quill Editor
-var quill = new Quill('#editor', {
-  theme: 'snow'
-});
--- ResumeApp/static/assets/js/pages/profile_edit.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($) {
-  $('.dropify').dropify({
-      messages: { 'default': 'Click to Upload or Drag n Drop', 'remove':  '<i class="flaticon-close-fill"></i>', 'replace': 'Upload or Drag n Drop' }
-  });
-});
